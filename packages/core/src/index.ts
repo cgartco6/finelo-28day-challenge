436266eb52c8ffb0ddd2dda66a5999a0a3caeabe
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-09 20:40:09 +0200

    Add exports for core modules and types

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export { ModuleRegistry } from './ModuleRegistry.js';
+export { TestRunner } from './TestRunner.js';
+export { SelfHealingTestRunner } from './SelfHealingTestRunner.js';
+export { MutationTester } from './MutationTester.js';
+export { AdaptivePlanner } from './AdaptivePlanner.js';
+export { AutonomousValidationCore } from './Orchestrator.js';
+export type { ModuleMetadata, TestResult, ValidationConfig } from './types.js';
