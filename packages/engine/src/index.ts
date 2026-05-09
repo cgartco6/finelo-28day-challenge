79ab459f6c5ec76840947a252de63e5ba67c73ba
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-09 19:57:23 +0200

    Create index.ts to export engine modules

---
+++ packages/engine/src/index.ts
@@ -0,0 +1,6 @@
+export { ModuleRegistry } from "./ModuleRegistry.js";
+export { TestRunner } from "./TestRunner.js";
+export { SelfHealingTestRunner } from "./SelfHealingTestRunner.js";
+export { MutationTester } from "./MutationTester.js";
+export { AdaptivePlanner } from "./AdaptivePlanner.js";
+export { AutonomousValidationCore } from "./Orchestrator.js";
