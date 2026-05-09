3e83a0b54f0aa2e5d071b239d271a542cd851c25
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-09 21:54:27 +0200

    Refactor exports in index.ts for approval package

--- packages/approval/src/index.ts
+++ packages/approval/src/index.ts
@@ -1,2 +1,9 @@
-export { ApprovalWorkflow } from "./ApprovalWorkflow.js";
-export { ApprovalRequest } from "./types.js";
+export { ApprovalWorkflow } from './ApprovalWorkflow.js';
+export type { 
+  ApprovalRequest, 
+  ApprovalFilters, 
+  ApprovalStats,
+  ApprovalAction,
+  ApprovalWebhook 
+} from './types.js';
+export { ApprovalStatus, ApprovalPriority } from './types.js';
