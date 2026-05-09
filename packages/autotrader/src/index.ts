25b313d8abca0cbbee9089dc718ea34537659889
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-09 21:44:03 +0200

    Add new trading strategies and types to index

--- packages/autotrader/src/index.ts
+++ packages/autotrader/src/index.ts
@@ -1,2 +1,12 @@
-export { AutoTraderEngine } from "./AutoTraderEngine.js";
-export { TradeSignal, ApprovalRequest, TradingStrategy } from "./types.js";
+export { AutoTraderEngine } from './AutoTraderEngine.js';
+export { MovingAverageCrossStrategy } from './strategies/MovingAverageCrossStrategy.js';
+export { RSIStrategy } from './strategies/RSIStrategy.js';
+export type { 
+  TradeSignal, 
+  ApprovalRequest, 
+  TradingStrategy, 
+  StrategyConfig,
+  RiskParameters,
+  PerformanceMetrics 
+} from './types.js';
+export { SignalStrength, OrderType, TimeInForce } from './types.js';
